fb722cb8915de18dad7579be7fa178a2e6b2f6f5
korynewton <korynewtondev@gmail.com>
2019-04-10 13:17:06 -0700

    test cannot be closed or open if locked

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 import Dashboard from '../dashboard/Dashboard';
+import Display from '../display/Display';
+
+afterEach(cleanup);
 
 describe('Gate tests', () => {
   it.skip('screenshots application', () => {
@@ -16,13 +20,14 @@ describe('Gate tests', () => {
     getByText(/unlocked/i);
     getByText(/open/i);
   });
+  it('cannot be closed or open if locked', () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    getByText(/Locked/);
+    getByText(/Closed/);
+  });
 });
 
 //ToDos:
-
-//Gate:
-// should default to unlocked and open
-// cannot be closed or open if unlocked
 
 //Dashboard:
 // shows controls and display
