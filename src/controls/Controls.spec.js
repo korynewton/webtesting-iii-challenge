ba9e44e5ab176015b87c4757a9a4d46d10296086
korynewton <korynewtondev@gmail.com>
2019-04-10 14:12:38 -0700

    checks that buttons to toggled locked and closed states are rendered

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { render, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
@@ -10,5 +9,20 @@ afterEach(cleanup);
 describe('<Controls />', () => {
   it('renders Controls component without error', () => {
     render(<Controls />);
+  });
+
+  it('provide buttons to toggle the closed and locked states.', () => {
+    const mockFn = jest.fn();
+    const { getByText } = render(
+      <Controls
+        locked={false}
+        closed={false}
+        toggleLocked={mockFn}
+        toggleClosed={mockFn}
+      />
+    );
+
+    expect(getByText(/lock gate/i)).toBeInTheDocument();
+    expect(getByText(/close gate/i)).toBeInTheDocument();
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import renderer from 'react-test-renderer';
 import { render, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
