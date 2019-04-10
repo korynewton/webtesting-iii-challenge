e3db02ec319ea16a654e88418b8485a5f34b1abb
korynewton <korynewtondev@gmail.com>
2019-04-10 15:28:43 -0700

    todo removed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -26,21 +26,3 @@ describe('Gate tests', () => {
     getByText(/Closed/);
   });
 });
-
-//ToDos:
-
-//Dashboard:
-// shows controls and display
-
-//Display:
-// displays if gate is open/closed and if it is locked/unlocked
-// displays 'Closed' if the closed prop is true and 'Open' if otherwise
-// displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
-// when locked or closed use the red-led class
-// when unlocked or open use the green-led class
-
-//Controls Component:
-// provide buttons to toggle the closed and locked states.
-// buttons' text changes to reflect the state the door will be in if clicked
-// the closed toggle button is disabled if the gate is closed
-// the locked toggle button is disabled if the gate is open
